b4cb985dc615dfb34d18d84245a1c26cc9e67922
Sneh Raj Lakhotia <srlakhotia@gmail.com>
2018-01-31 10:36:14 +0530

    react integration

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = 8080;
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/fiddleBoard', (err) => {
+mongoose.connect('mongodb://localhost:27017/RaceToDone', (err) => {
     if(err) {
         return err;
     }
@@ -38,6 +38,11 @@ app.use('/graphql', bodyParser.json(), apolloExpress({
 app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql'
 }));
+
+app.set('views', __dirname + '/src');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.get('/', require('./src/routes').index);
 
 app.listen(PORT, () => {
     console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphql`)
